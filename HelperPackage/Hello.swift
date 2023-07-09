499646ffe0b99a45d4cbbaa165be164a7f3488b4
radhekrishna <kalsariya083@gmail.com>
2023-07-09 12:02:55 +0530

    first commit

---
+++ HelperPackage/Hello.swift
@@ -0,0 +1,16 @@
+//
+//  Hello.swift
+//  HelperPackage
+//
+//  Created by RadheKrishna on 09/07/23.
+//
+
+import Foundation
+
+class Hello {
+    
+    func helloPrinter() {
+        print("Ram")
+    }
+    
+}
---
+++ HelperPackage/HelperPackage.h
@@ -0,0 +1,18 @@
+//
+//  HelperPackage.h
+//  HelperPackage
+//
+//  Created by RadheKrishna on 09/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelperPackage.
+FOUNDATION_EXPORT double HelperPackageVersionNumber;
+
+//! Project version string for HelperPackage.
+FOUNDATION_EXPORT const unsigned char HelperPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelperPackage/PublicHeader.h>
+
+
